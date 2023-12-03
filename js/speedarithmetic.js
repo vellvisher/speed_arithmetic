986f8b9de99946abf91120c879681d7b41c42ea9
Vaarnan Drolia <vaarnan@gmail.com>
2023-12-02 17:48:57 -0800

    Make js relative and load the exercises.json locally

--- js/speedarithmetic.js
+++ js/speedarithmetic.js
@@ -1,3 +1,67 @@
+var json = [
+    {
+        "id": "10",
+        "number": 17,
+        "operator": "+"
+    },
+    {
+        "id": "24",
+        "number": 23,
+        "operator": "+"
+    },
+    {
+        "id": "25",
+        "number": 24,
+        "operator": "+"
+    },
+    {
+        "id": "28",
+        "number": 25,
+        "operator": "+"
+    },
+    {
+        "id": "29",
+        "number": 26,
+        "operator": "+"
+    },
+    {
+        "id": "30",
+        "number": 27,
+        "operator": "+"
+    },
+    {
+        "id": "52",
+        "number": 6,
+        "operator": "*"
+    },
+    {
+        "id": "53",
+        "number": 7,
+        "operator": "*"
+    },
+    {
+        "id": "56",
+        "number": 8,
+        "operator": "*"
+    },
+    {
+        "id": "60",
+        "number": 9,
+        "operator": "*"
+    },
+    {
+        "id": "61",
+        "number": 11,
+        "operator": "*"
+    },
+    {
+        "id": "77",
+        "number": 12,
+        "operator": "*"
+    }
+];
+
+
 $(function() {
     CURRENT_INDEX = 0;
     $("#start-button").hide();
@@ -5,7 +69,7 @@ $(function() {
     $("#exercise-screen").hide();
     EXERCISES = {};
     var DATA_URL = "https://dl.dropboxusercontent.com/u/33075650/exercises.json";
-    $.getJSON(DATA_URL, function(json) {
+    // $.getJSON(DATA_URL, function(json) {
         temp = json;
         $.each(json, function(index, exercise) {
             EXERCISES["Exercise " + exercise.id] = exercise;
@@ -23,7 +87,7 @@ $(function() {
             $("#start-button").show();
         });
         $("#exercise").prop("selectedIndex", -1);
-    });
+    // });
     $("#start-button").click(function() {
         SELECTED_EXERCISE.data = shuffle(range(1, 100));
         $("#data-number").text(SELECTED_EXERCISE.data[CURRENT_INDEX]);
